334f4ada60b2c07e23046a4e7355f90a58f61162
sazan10 <33653716+sazan10@users.noreply.github.com>
2024-09-22 16:40:13 +0545

    Update safe.c

--- safe.c
+++ safe.c
@@ -1,8 +1,8 @@
-
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void signal_handler(int signo) {
     printf("Signal %d received by thread %ld\n", signo, pthread_self());
@@ -16,25 +16,42 @@ void* thread_func(void* arg) {
     return NULL;
 }
 
+void alarm_handler(int signo) {
+    printf("Program terminating after timeout.\n");
+    exit(0);  // Exit the program cleanly
+}
+
 int main() {
     pthread_t threads[4];
     struct sigaction sa;
 
+    // Set up signal handler for SIGPROF
     sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(SIGPROF, &sa, NULL);
 
+    // Set up signal handler for alarm timeout (SIGALRM)
+    struct sigaction alarm_sa;
+    alarm_sa.sa_handler = alarm_handler;
+    sigemptyset(&alarm_sa.sa_mask);
+    alarm_sa.sa_flags = 0;
+    sigaction(SIGALRM, &alarm_sa, NULL);
+
+    // Create threads
     for (int i = 0; i < 4; i++) {
         pthread_create(&threads[i], NULL, thread_func, NULL);
     }
 
+    // Set an alarm to terminate the program after 10 seconds
+    alarm(100);
+
+    // Send signals to threads in a loop
     while (1) {
-        // Send SIGPROF signals to threads
         for (int i = 0; i < 4; i++) {
             pthread_kill(threads[i], SIGPROF);
         }
-        sleep(1);
+        sleep(1);  // Avoid tight looping
     }
 
     return 0;
